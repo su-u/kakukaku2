9a15ceeecd807e4347fe53436175f3c90586e455
su-u <aaaiiiuuu35@gmail.com>
2017-07-15 23:02:40 +0900

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(void)
+{
+	int s[100][100] = { 0 };
+	int i, j, i_max;
+	int ookii;
+	//ƒtƒ@ƒCƒ‹
+	FILE *fp;
+	if (fopen_s(&fp, "input.txt", "r") == NULL) {
+		fscanf_s(fp, "%d", &i_max);
+		for (i = 0; i < i_max; i++) {
+			for (j = 0; j < i+ 1; j++)
+				fscanf_s(fp, "%d", &s[i][j]);
+		}
+		fclose(fp);
+	}
+
+	for (i = i_max - 1; i >= 0; i--) {
+		for (j = 0; j < i + 1 ; j++) {
+			//printf("%d,%d\n", s[i][j], s[i][j + 1]);
+			if (s[i + 1][j] >= s[i + 1][j + 1]) {
+				ookii = s[i + 1][j];
+			} else {
+				ookii = s[i + 1][j + 1];
+			}
+			s[i][j] += ookii;
+		}
+	}
+	printf("most=%d\n", s[0][0]);
+}
